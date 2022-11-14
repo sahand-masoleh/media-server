123167fbff7ecaffc1c8a4406e52cd254c4c3b79
Sahand <sahand.masoleh@gmail.com>
2022-11-14 19:38:14 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,69 @@
+const { PORT, NODE_ENV, ORIGIN } = process.env;
+
+import express, { ErrorRequestHandler } from "express";
+const app = express();
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** CORS configuration */
+
+import cors from "cors";
+app.use(
+	cors({
+		origin:
+			NODE_ENV === "development"
+				? true
+				: NODE_ENV === "production"
+				? ORIGIN
+				: false,
+	})
+);
+
+/** serve static files */
+app.use("/media", express.static(path.join(__dirname, "../media")));
+
+/** root route */
+
+app.get("/", (req, res) => {
+	// TODO: serve the frontend in production
+	res.send("nothing to see here");
+});
+
+/** not found */
+
+app.use((req, res) => {
+	res.status(404);
+	res.json({ success: false, message: "not found" });
+});
+
+/** error handler */
+
+interface Errorable {
+	status: number;
+	message: string;
+}
+
+const errorHandler: ErrorRequestHandler = function (
+	error: Errorable,
+	req,
+	res,
+	next
+) {
+	console.error(error.message);
+	res.status(error.status || 500);
+	res.json({
+		success: false,
+		message: error.message || "internal server error",
+	});
+};
+
+app.use(errorHandler);
+
+/** start the server */
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
