4a3d894371e48d70e82b291e897bfafb8bdb9870
Sahand <sahand.masoleh@gmail.com>
2022-12-06 21:47:23 +0100

    refractor: removed typescript

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 const { PORT, NODE_ENV, ORIGIN } = process.env;
-import express from "express";
+const express = require("express");
 const app = express();
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-import cors from "cors";
-app.use(cors({
-    origin: NODE_ENV === "development"
-        ? true
-        : NODE_ENV === "production"
-            ? ORIGIN
-            : false,
-}));
+const path = require("path");
+
+/** CORS configuration */
+const cors = require("cors");
+
+app.use(
+	cors({
+		origin:
+			NODE_ENV === "development"
+				? true
+				: NODE_ENV === "production"
+				? ORIGIN
+				: false,
+	})
+);
+app.use(cors());
+
+/** serve static files */
 app.use("/media", express.static(path.join(__dirname, "/media")));
+
+/** root route */
 app.get("/", (req, res) => {
-    res.send("nothing to see here");
+	res.send("nothing to see here");
 });
 app.use((req, res) => {
-    res.status(404);
-    res.json({ success: false, message: "not found" });
+	res.status(404);
+	res.json({ success: false, message: "not found" });
 });
-const errorHandler = function (error, req, res, next) {
-    console.error(error.message);
-    res.status(error.status || 500);
-    res.json({
-        success: false,
-        message: error.message || "internal server error",
-    });
-};
-app.use(errorHandler);
+
+/** error handler */
+app.use((error, req, res, next) => {
+	console.error(error.message);
+	res.status(500);
+	res.json({
+		success: false,
+		message: error.message || "internal server error",
+	});
+});
+
+/** start the server */
 app.listen(PORT, () => {
-    if (NODE_ENV === "development") {
-        console.log(`listening on port ${PORT}`);
-    }
+	if (NODE_ENV === "development") {
+		console.log(`listening on port ${PORT}`);
+	}
 });
